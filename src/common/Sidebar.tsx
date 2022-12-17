51f02e03b769e3f506f8f30cc4f42e8b9f69098c
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-18 06:12:59 +0700

    update sidebar

--- src/common/Sidebar.tsx
+++ src/common/Sidebar.tsx
@@ -1,17 +1,6 @@
 export default function SidebarCommon() {
   return (
     <aside className="main-sidebar sidebar-dark-primary elevation-4">
-      {/* Brand Logo */}
-      <a href="index3.html" className="brand-link">
-        <img
-          src="dist/img/AdminLTELogo.png"
-          alt="AdminLTE Logo"
-          className="brand-image img-circle elevation-3"
-          style={{ opacity: ".8" }}
-        />
-        <span className="brand-text font-weight-light">AdminLTE 3</span>
-      </a>
-      {/* Sidebar */}
       <div className="sidebar">
         {/* Sidebar user panel (optional) */}
         <div className="user-panel mt-3 pb-3 mb-3 d-flex">
@@ -28,22 +17,6 @@ export default function SidebarCommon() {
             </a>
           </div>
         </div>
-        {/* SidebarSearch Form */}
-        <div className="form-inline">
-          <div className="input-group" data-widget="sidebar-search">
-            <input
-              className="form-control form-control-sidebar"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <div className="input-group-append">
-              <button className="btn btn-sidebar">
-                <i className="fas fa-search fa-fw" />
-              </button>
-            </div>
-          </div>
-        </div>
         {/* Sidebar Menu */}
         <nav className="mt-2">
           <ul
@@ -55,19 +28,13 @@ export default function SidebarCommon() {
             <li className="nav-item menu-open">
               <a href="#" className="nav-link active">
                 <i className="nav-icon fas fa-tachometer-alt" />
-                <p>
-                  Dashboard
-                  <i className="right fas fa-angle-left" />
-                </p>
+                <p>สรุปยอดขายทั้งหมด</p>
               </a>
             </li>
             <li className="nav-item">
               <a href="pages/widgets.html" className="nav-link">
                 <i className="nav-icon fas fa-th" />
-                <p>
-                  Widgets
-                  <span className="right badge badge-danger">New</span>
-                </p>
+                <p>ตัวอย่าง</p>
               </a>
             </li>
             <li className="nav-item">
