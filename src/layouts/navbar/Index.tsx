f89fa661c50c4251510df3c88adaaf131527c3a2
กีตาร์เอาเอง <pppppppppp102014@hotmail.com>
2022-12-18 11:22:35 +0700

    :guitar: refactor navbar

--- src/layouts/navbar/Index.tsx
+++ src/layouts/navbar/Index.tsx
@@ -1,11 +1,66 @@
-import LeftNav from "./LeftNav";
-import RightNav from "./RightNav";
-
 export default function LayoutNav() {
   return (
     <>
-      <LeftNav />
-      <RightNav />
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a
+            className="nav-link"
+            data-widget="pushmenu"
+            href="#test"
+            role="button"
+          >
+            <i className="fas fa-bars" />
+          </a>
+        </li>
+      </ul>
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <a className="nav-link" href="#test" role="button">
+            <i className="fas fa-envelope mx-2" />
+            CONTACT@DEV.COM
+          </a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link" data-toggle="dropdown" href="#test">
+            <i className="far fa-bell" />
+            <span className="badge badge-warning navbar-badge">15</span>
+          </a>
+          <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+            <span className="dropdown-item dropdown-header">
+              15 Notifications
+            </span>
+            <div className="dropdown-divider" />
+            <a href="#test" className="dropdown-item">
+              <i className="fas fa-envelope mr-2" /> 4 new messages
+              <span className="float-right text-muted text-sm">3 mins</span>
+            </a>
+            <div className="dropdown-divider" />
+            <a href="#test" className="dropdown-item">
+              <i className="fas fa-users mr-2" /> 8 friend requests
+              <span className="float-right text-muted text-sm">12 hours</span>
+            </a>
+            <div className="dropdown-divider" />
+            <a href="#test" className="dropdown-item">
+              <i className="fas fa-file mr-2" /> 3 new reports
+              <span className="float-right text-muted text-sm">2 days</span>
+            </a>
+            <div className="dropdown-divider" />
+            <a href="#test" className="dropdown-item dropdown-footer">
+              See All Notifications
+            </a>
+          </div>
+        </li>
+        <li className="nav-item">
+          <a
+            className="nav-link"
+            data-widget="fullscreen"
+            href="#test"
+            role="button"
+          >
+            <i className="fas fa-expand-arrows-alt" />
+          </a>
+        </li>
+      </ul>
     </>
   );
 }
