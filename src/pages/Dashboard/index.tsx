22101c98ab2a8bd39abe321b132255ecdda28bb2
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-19 10:28:36 +0700

    Update index.tsx

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,8 @@
 import MainContent from "../../common/MainContent";
-import NavBarCommon from "../../common/NavBar";
-import SidebarCommon from "../../common/Sidebar";
 
 export default function Dashboard() {
   return (
     <div className="wrapper">
-      <NavBarCommon />
-      <SidebarCommon />
       <MainContent />
     </div>
   );
