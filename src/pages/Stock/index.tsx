f44b35cf473cf8bff0aa5a3f5c8ae8b727c8c19d
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-19 10:08:41 +0700

    move navbarcommon&&sidebarcommon

--- src/pages/Stock/index.tsx
+++ src/pages/Stock/index.tsx
@@ -1,12 +1,8 @@
-import NavBarCommon from "../../common/NavBar";
-import SidebarCommon from "../../common/Sidebar";
 import Stock from "../../common/Stock";
 
 export default function StockPage() {
   return (
     <div className="wrapper">
-      <NavBarCommon />
-      <SidebarCommon />
       <Stock />;
     </div>
   );
