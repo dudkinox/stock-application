c82614c4fcb3fe73709d2652fd4c5653ca28009d
กีตาร์เอาเอง <pppppppppp102014@hotmail.com>
2022-12-30 19:16:52 +0700

    Update index.php

--- backend/apis/stocks/insert/index.php
+++ backend/apis/stocks/insert/index.php
@@ -3,6 +3,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Credentials: true");
+// header("Access-Control-Allow-Headers: X-Custom-Header");
 require('../../../client/index.php');
 
 $requestBody = json_decode(file_get_contents('php://input'), true);
