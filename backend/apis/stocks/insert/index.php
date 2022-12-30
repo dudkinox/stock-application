bd81b70b05a3dfde3981322c65b4f08cad3ae2ad
กีตาร์เอาเอง <pppppppppp102014@hotmail.com>
2022-12-30 18:42:44 +0700

    :guitar: fix : api insert

--- backend/apis/stocks/insert/index.php
+++ backend/apis/stocks/insert/index.php
@@ -1,6 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 header('Content-Type: application/json; charset=utf-8');
 require('../../../client/index.php');
