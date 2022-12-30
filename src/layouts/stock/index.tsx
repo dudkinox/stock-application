f1f026cef74534e6fa53d3948bea41bd80929c9b
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-31 05:35:13 +0700

    :HEEPOKE:Get Stock Api

--- src/layouts/stock/index.tsx
+++ src/layouts/stock/index.tsx
@@ -10,6 +10,7 @@ export default function StockLayout() {
   useEffect(() => {
     Https.get("/apis/stocks/get")
       .then((res: any) => {
+        setStock(res.data);
         console.log(res);
       })
       .catch((err) => {
@@ -17,48 +18,85 @@ export default function StockLayout() {
       });
   }, []);
 
-  return (
-    <>
-      <div className="card-body">
-        <table
-          id="stockTable"
-          className="table table-bordered table-hover dtr-inline collapsed w-100"
-        >
-          <thead>
-            <tr className="text-center">
-              <th>รหัสเอกสาร</th>
-              <th>
-                <InsertModal />
-              </th>
-              <th>เลขบัตรประชาชน</th>
-              <th>ประวัติลูกค้า</th>
-              <th>ประเภท</th>
-              <th>รายละเอียด</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="text-center">
-              <td>1</td>
-              <td>
-                <div className="row justify-content-center">
-                  <button className="btn btn-warning mx-2">
-                    <i className="nav-icon fas fa-pen" />
-                  </button>
-                  <button className="btn btn-danger">
-                    <i className="nav-icon fas fa-trash" />
-                  </button>
-                </div>
-              </td>
-              <td>Internet Explorer 4.0</td>
-              <td>Win 95+</td>
-              <td> 4</td>
-              <td>
-                <DetailModal />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+  if (stock.length >= 1) {
+    return (
+      <>
+        <div className="card-body">
+          <table
+            id="stockTable"
+            className="table table-bordered table-hover dtr-inline collapsed w-100"
+          >
+            <thead>
+              <tr className="text-center">
+                <th>รหัสเอกสาร</th>
+                <th>
+                  <InsertModal />
+                </th>
+                <th>เลขบัตรประชาชน</th>
+                <th>ประวัติลูกค้า</th>
+                <th>ประเภท</th>
+                <th>รายละเอียด</th>
+              </tr>
+            </thead>
+            <tbody>
+              {stock.map((item, i) => {
+                return (
+                  <tr key={i} className="text-center">
+                    <td>รหัสเอกสาร</td>
+                    <td>
+                      <div className="row justify-content-center">
+                        <button className="btn btn-warning mx-2">
+                          <i className="nav-icon fas fa-pen" />
+                        </button>
+                        <button className="btn btn-danger">
+                          <i className="nav-icon fas fa-trash" />
+                        </button>
+                      </div>
+                    </td>
+                    <td>{item.idCard}</td>
+                    <td>{item.customerStatus}</td>
+                    <td>{item.stockType}</td>
+                    <td>
+                      <DetailModal />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="card-body">
+          <table
+            id="stockTable"
+            className="table table-bordered table-hover dtr-inline collapsed w-100"
+          >
+            <thead>
+              <tr className="text-center">
+                <th>รหัสเอกสาร</th>
+                <th>
+                  <InsertModal />
+                </th>
+                <th>เลขบัตรประชาชน</th>
+                <th>ประวัติลูกค้า</th>
+                <th>ประเภท</th>
+                <th>รายละเอียด</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="text-center">
+                <td colSpan={6}>
+                  <h2 className="mt-2">ยังไม่มีข้อมูล</h2>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  }
 }
