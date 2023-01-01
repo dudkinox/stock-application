d590da3f487d15da3724a9af71ff78ed413b5c36
กีตาร์เอาเอง <pppppppppp102014@hotmail.com>
2023-01-02 02:31:30 +0700

    Update DataTable.ts

--- src/common/DataTable.ts
+++ src/common/DataTable.ts
@@ -1,18 +1,20 @@
 export default function initTable() {
   ($("#stock-table") as any).DataTable({
+    responsive: true,
+    autoWidth: true,
     paging: true,
     lengthChange: true,
     searching: true,
     ordering: true,
     info: true,
-    autoWidth: false,
-    responsive: true,
     bDestroy: false,
     stateSave: true,
     retrieve: true,
     order: [[0, "desc"]],
     language: {
       zeroRecords: "ไม่พบข้อมูล",
+      infoEmpty: "ไม่พบข้อมูล",
+      infoFiltered: "(กรองจากทั้งหมด MAX รายการ)",
       search: "ค้นหา:",
       loadingRecords: "รอสักครู่",
       paginate: {
