e72c30ac2f421056367272aa2d7e6f2557de2ca4
กีตาร์เอาเอง <pppppppppp102014@hotmail.com>
2023-01-03 23:53:20 +0700

    :guitar: backend update stock

--- backend/apis/stocks/update/index.php
+++ backend/apis/stocks/update/index.php
@@ -1,10 +1,75 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
 require('../../../client/index.php');
 
 $id = isset($_GET['id']) ? $_GET['id'] : '';
-$date = isset($_GET['date']) ? $_GET['date'] :
+$stockType = isset($_GET['stock_type']) ? $_GET['stock_type'] :
 
-    $query = "UPDATE stock 
-SET `DATE`='" . $date . "'
-WHERE ID = '" . $_GET["id"] . "'";
+    $requestBody = json_decode(file_get_contents('php://input'), true);
+
+$queryIdCard = "SELECT ID_CARD FROM stock WHERE ID = '" . $id . "'";
+$resultIdCard = $conn->query($queryIdCard);
+$idCard = $resultIdCard->fetch_assoc()["ID_CARD"];
+
+$queryUpdateDate = "UPDATE stock 
+SET `DATE`='" . $requestBody["date"] . "'
+WHERE ID = '" . $id . "'";
+
+$queryUpdateStock = "";
+switch ($stockType) {
+    case 'อุปกรณ์':
+        $queryUpdateStock = "UPDATE equipment SET 
+        CASES='" . $requestBody["cases"] . "',
+        FIRM='" . $requestBody["firm"] . "',
+        `LEN`='" . $requestBody["len"] . "',
+        BIG_CHARGE='" . $requestBody["big_charge"] . "',
+        CHARGE='" . $requestBody["charge"] . "',
+        REPAIR='" . $requestBody["repair"] . "',
+        `SUM`='" . $requestBody["sum"] . "' 
+        WHERE ID_CARD = '" . $idCard . "'";
+        break;
+    case 'ผ่อน':
+        $queryUpdateStock = "UPDATE installment_payment SET 
+        INSTALLMENT_NO='" . $requestBody["installment_no"] . "',
+        PRICE_TOTAL='" . $requestBody["price_total"] . "'
+        WHERE ID_CARD = '" . $idCard . "'";
+        break;
+    case 'ขาย':
+        $queryUpdateStock = "UPDATE kay SET 
+        CUSTOMER='" . $requestBody["customer"] . "',
+        TEL='" . $requestBody["tel"] . "',
+        `VERSION`='" . $requestBody["version"] . "',
+        IMEI='" . $requestBody["imei"] . "',
+        STAR_MONEY='" . $requestBody["star_money"] . "',
+        `MONTH`='" . $requestBody["month"] . "',
+        INSTALLMENT='" . $requestBody["installment"] . "',
+        DATE_PAYMENT='" . $requestBody["date_payment"] . "' 
+        WHERE ID_CARD = '" . $idCard . "'";
+        break;
+    case 'ซื้อ':
+        $queryUpdateStock = "UPDATE `bye` SET 
+        `VERSION`='" . $requestBody["version"] . "',
+        PRICE='" . $requestBody["price"] . "',
+        IMEI='" . $requestBody["imei"] . "',
+        SOURCE='" . $requestBody["source"] . "',
+        BATTERY='" . $requestBody["battery"] . "' 
+        WHERE ID_CARD = '" . $idCard . "'";
+        break;
+}
+
+if (
+    $conn->query($queryUpdateDate) === TRUE &&
+    $conn->query($queryUpdateStock) === TRUE
+) {
+    echo "{ \"status\": \"success\",
+        \"message\": \"แก้ไขข้อมูลสำเร็จ\",
+        \"code\": \"000\" }";
+} else {
+    echo "{ \"status\": \"error\",
+        \"message\": \"Error: " . $queryUpdateStock . "<br>" . $conn->error . "\",
+        \"code\": \"001\" }";
+}
