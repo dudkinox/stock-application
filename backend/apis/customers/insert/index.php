269cfbd51d41c7a526dd92eca7e4e6311a335852
กีตาร์เอาเอง <pppppppppp102014@hotmail.com>
2023-01-07 18:21:30 +0700

    :guitar: set header CORS

--- backend/apis/customers/insert/index.php
+++ backend/apis/customers/insert/index.php
@@ -1,5 +1,7 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
 require('../../../client/index.php');
 
