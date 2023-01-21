a5889567b6395d874ddf2975e2e25ab6a4e702f5
กีตาร์เอาเอง <45527907+dudkinox@users.noreply.github.com>
2023-01-22 00:36:05 +0700

    :guitar: api insert account

---
+++ backend/apis/accounts/insert/index.php
@@ -0,0 +1,27 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
+require('../../../client/index.php');
+
+$requestBody = json_decode(file_get_contents('php://input'), true);
+
+$query = "INSERT INTO user(USERNAME, 
+                        `PASSWORD`, 
+                        MAJOR, 
+                        PERMISSION) 
+        VALUES ('" . $requestBody["username"] . "',
+                '" . $requestBody["password"] . "',
+                '" . $requestBody["major"] . "',
+                '" . $requestBody["permission"] . "')";
+
+if ($conn->query($query) === TRUE) {
+    echo "{ \"status\": \"success\",
+        \"message\": \"เพิ่มข้อมูลสำเร็จ\",
+        \"code\": \"000\" }";
+} else {
+    echo "{ \"status\": \"error\",
+        \"message\": \"Error: " . $query . "<br>" . $conn->error . "\",
+        \"code\": \"001\" }";
+}
