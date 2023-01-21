89299a25e9b5dc428ac2a1dcb350ff2121c77a9d
กีตาร์เอาเอง <45527907+dudkinox@users.noreply.github.com>
2023-01-22 00:39:32 +0700

    :guitar: api get accounts

---
+++ backend/apis/accounts/get/index.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+require('../../../client/index.php');
+
+$query = "SELECT * FROM user";
+$result = $conn->query($query);
+
+$i = 1;
+echo '[';
+while ($row = $result->fetch_assoc()) {
+    echo '
+        {
+        "ID": "' . $row["ID"] . '",
+        "USERNAME": "' . $row["USERNAME"] . '",
+        "PASSWORD": "' . $row["PASSWORD"] . '",
+        "MAJOR": "' . $row["MAJOR"] . '",
+        "PERMISSION": "' . $row["PERMISSION"] . '"
+        }';
+    if ($i != $result->num_rows) {
+        echo ',';
+    }
+    $i++;
+}
+echo ']';
