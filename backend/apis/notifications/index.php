4ba82a07e07cce558853d1a8764c44ca5cc842d1
กีตาร์เอาเอง <guitar@odds.team>
2023-02-18 13:11:22 +0700

    :guitar: custom time line

--- backend/apis/notifications/index.php
+++ backend/apis/notifications/index.php
@@ -3,6 +3,7 @@ header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
 $message = isset($_GET['message']) ? $_GET['message'] : '';
+$notificationTime = isset($_GET['notificationTime']) ? $_GET['notificationTime'] : '';
 
 $curl = curl_init();
 
@@ -15,7 +16,7 @@ curl_setopt_array($curl, array(
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS => 'message=' . $message,
+    CURLOPT_POSTFIELDS => 'message=' . $message . '&notificationTime=' . $notificationTime,
     CURLOPT_HTTPHEADER => array(
         'Authorization: Bearer AALDtW9mChJkQrVKbxkwwHUG27lNxiMhhCGnhfCQvtT',
         'Content-Type: application/x-www-form-urlencoded'
