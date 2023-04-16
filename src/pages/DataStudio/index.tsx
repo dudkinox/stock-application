d294e163d423f269e0b8955cf4d779f21347886a
กีตาร์เอาเอง <guitar@odds.team>
2023-04-16 21:33:05 +0700

    :guitar:

--- src/pages/DataStudio/index.tsx
+++ src/pages/DataStudio/index.tsx
@@ -1,26 +1,13 @@
 export default function DataStudio() {
-    return (
+  return (
+    <section className="content">
       <div className="container-fluid">
-        <div className="row">
-          <div className="col-12">
-            <div className="card">
-              <div className="card-header">
-                <h3 className="card-title">Data Studio</h3>
-              </div>
-              <div className="card-body">
-                <div className="embed-responsive embed-responsive-16by9">
-                  <iframe
-                    title="Data Studio Embed"
-                    className="embed-responsive-item"
-                    src="https://lookerstudio.google.com/u/0/reporting/70820d12-7285-4e25-a251-b0ab96e4e583/page/lBzMD"
-                    allowFullScreen
-                  ></iframe>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <iframe
+          src="https://lookerstudio.google.com/s/hrJga2ftX88"
+          width="100%"
+          height={600}
+        />
       </div>
-    );
-  }
-  
+    </section>
+  );
+}
