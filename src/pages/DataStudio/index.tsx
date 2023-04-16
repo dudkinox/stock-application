07cdc1b84cc409ed20efce1b7d2818778b998447
กีตาร์เอาเอง <guitar@odds.team>
2023-04-16 21:42:09 +0700

    ซเีร

--- src/pages/DataStudio/index.tsx
+++ src/pages/DataStudio/index.tsx
@@ -3,9 +3,12 @@ export default function DataStudio() {
     <section className="content">
       <div className="container-fluid">
         <iframe
-          src="https://lookerstudio.google.com/s/hrJga2ftX88"
-          width="100%"
-          height={600}
+          width={600}
+          height={450}
+          src="https://lookerstudio.google.com/embed/reporting/70820d12-7285-4e25-a251-b0ab96e4e583/page/lBzMD"
+          frameBorder={0}
+          style={{ border: 0 }}
+          allowFullScreen
         />
       </div>
     </section>
