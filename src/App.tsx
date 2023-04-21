af28b0d26b1eac7409b2e53316c41057dd75f85b
กีตาร์เอาเอง <guitar@odds.team>
2023-04-21 13:30:44 +0700

    :guitar:

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import LoginPage from "./pages/LoginPage";
 import { useContext, useEffect } from "react";
 import { AppContext } from "./contexts";
 import { PermissionEnum } from "./enum/permission.enum";
-import { DashBoardContextProvider } from "./contexts/DashBoardContext";
 import DataStudio from "./pages/DataStudio";
 
 export default function App() {
@@ -30,22 +29,7 @@ export default function App() {
       <SidebarCommon />
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <DashBoardContextProvider>
-                <Dashboard />
-              </DashBoardContextProvider>
-            }
-          />
-          <Route
-            path="/data-studio"
-            element={
-              <DashBoardContextProvider>
-                <DataStudio />
-              </DashBoardContextProvider>
-            }
-          />
+          <Route path="/" element={<DataStudio />} />
           <Route
             path="/stock"
             element={
