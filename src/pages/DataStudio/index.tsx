7db63c1a5640bbd401f0eb6be2ed9b6b006483f2
กีตาร์เอาเอง <guitar@odds.team>
2023-04-23 17:53:32 +0700

    Update index.tsx

--- src/pages/DataStudio/index.tsx
+++ src/pages/DataStudio/index.tsx
@@ -1,22 +1,21 @@
 export default function DataStudio() {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-12">
-          <div className="card">
-            <div className="card-header">
-              <h3 className="card-title">Data Studio</h3>
-            </div>
-            <div className="card-body">
-              <div className="embed-responsive embed-responsive-16by9">
-                <iframe
-                  width={600}
-                  height={350}
-                  src="https://lookerstudio.google.com/embed/reporting/70820d12-7285-4e25-a251-b0ab96e4e583/page/lBzMD"
-                  frameBorder={0}
-                  style={{ border: 0 }}
-                  allowFullScreen
-                />
+    <div className="content-wrapper">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <div className="card">
+              <div className="card-header">
+                <h3 className="card-title">Dashboard</h3>
+              </div>
+              <div className="card-body">
+                <div className="embed-responsive embed-responsive-16by9">
+                  <iframe
+                    src="https://lookerstudio.google.com/embed/reporting/70820d12-7285-4e25-a251-b0ab96e4e583/page/lBzMD"
+                    style={{ border: 0 }}
+                    allowFullScreen
+                  />
+                </div>
               </div>
             </div>
           </div>
