f1f6ae3a0ee6290736215d4a868cc7bdea5257a6
กีตาร์เอาเอง <guitar@odds.team>
2023-05-18 23:24:27 +0700

    Create index.php

---
+++ backend/apis/dashboard/get/index.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+require('../../../client/index.php');
+
+$query = "SELECT DISTINCT STOCK_TYPE FROM stock";
+$result = $conn->query($query);
+
+$i = 1;
+echo '[';
+while ($row = $result->fetch_assoc()) {
+    echo '
+        {
+        "STOCK_TYPE": "' . $row["STOCK_TYPE"] . '"
+        }';
+    if ($i != $result->num_rows) {
+        echo ',';
+    }
+    $i++;
+}
+echo ']';
