7f1f2b646f91315747e7fb2152fc4ff5181360d3
ศุภณัฐ ชินราช <45527907+dudkinox@users.noreply.github.com>
2023-09-03 13:55:17 +0700

    asd

--- src/pages/Stock/bye.tsx
+++ src/pages/Stock/bye.tsx
@@ -3,23 +3,31 @@ import TableCommon from "../../common/Table";
 import ContentLayOut from "../../layouts/ContentLayOut";
 import StockService from "../../services/StockServices";
 import { AppContext } from "../../contexts";
+import initTable, { destroyTable } from "../../common/DataTable";
 export function StockByePage() {
-  const { majorUser } = useContext(AppContext);
-  const [stock, setStock] = useState<any[]>([]);
-  const stockTableHeaders = [
-    "เลขบัตรประชาชน / ชื่อลูกค้า",
+  const { majorUser, setIsLoading } = useContext(AppContext);
+  const [buyList, setBuyList] = useState<any[]>([]);
+  const stockBuyListTableHeaders = [
+    "รหัสเอกสาร",
     "สาขา",
     "Serial Number",
     "รุ่น",
     "imei",
     "แหล่งที่มา",
     "Battery",
-    "สถานะ",
+    "ขาย",
   ];
 
   useEffect(() => {
-    StockService.GetStockKay(majorUser).then((res) => {
-      setStock(res.data);
+    setIsLoading(true);
+    StockService.GetStockBye(majorUser).then((res) => {
+      destroyTable();
+      setBuyList(res.data);
+      setTimeout(
+        () => initTable(res.data.length.toString() ?? "0", "#kay-table"),
+        100
+      );
+      setIsLoading(false);
     });
   }, []);
 
@@ -27,22 +35,22 @@ export function StockByePage() {
     <>
       <ContentLayOut
         title={"stock"}
-        topic={"สต๊อกสินค้า"}
+        topic={"สต๊อกสินค้า ที่ซื้อ"}
         page={
           <>
             <div className="card-body">
               <TableCommon
-                columns={stockTableHeaders}
-                row={stock.map((item, i) => (
+                columns={stockBuyListTableHeaders}
+                row={buyList.map((item, i) => (
                   <tr key={i} className="text-center">
-                    <td>{item.ID_CARD}</td>
+                    <td>{item.ID}</td>
                     <td>{item.MAJOR}</td>
                     <td>{item.SERIAL_NUMBER}</td>
                     <td>{item.VERSION}</td>
                     <td>{item.IMEI}</td>
                     <td>{item.SOURCE}</td>
                     <td>{item.BATTERY}</td>
-                    <td>{item.STATUS}</td>
+                    <td>{item.STATUS === "0" ? "ยังไม่ขาย" : "ขายไปแล้ว"}</td>
                   </tr>
                 ))}
               />
