212e8b2d327cdf40dfe26d410c44138405ba42c2
กีตาร์เอาเอง <guitar@odds.team>
2023-10-07 13:02:49 +0700

    Update spec.cy.ts

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,18 +1,18 @@
-describe("Recording 10/6/2023 at 1:12:20 PM", () => {
-  it("tests Recording 10/6/2023 at 1:12:20 PM", () => {
-    cy.viewport(540, 737);
+describe("search", () => {
+  it("tests search", () => {
+    cy.viewport(846, 737);
     cy.visit("https://www.google.com/");
     cy.get("#APjFqb").click();
-    cy.get("#APjFqb").type("guitar");
+    cy.get("#APjFqb").type("จองตั๋วดูหนัง");
     cy.get("div.UUbT9 input.gNO89b").click();
     cy.location("href").should(
       "eq",
-      "https://www.google.com/search?q=guitar&sca_esv=571184275&sxsrf=AM9HkKnjC52J02Qnkw8_TehL2mIRJHIQyg%3A1696572541222&source=hp&ei=faQfZYjKCpKR4-EP9v-CkAs&iflsig=AO6bgOgAAAAAZR-yjbrbyY6u30zgT22jM-QECmXMHCTm&ved=0ahUKEwjI_Jud4eCBAxWSyDgGHfa_ALIQ4dUDCAk&oq=guitar&gs_lp=Egdnd3Mtd2l6IgZndWl0YXIyBxAjGIoFGCcyDhAAGIoFGLEDGIMBGJECMggQABiKBRiSAzIIEAAYigUYkgMyDRAAGIAEGBQYhwIYsQMyCBAAGMsBGIAEMggQLhjLARiABDIIEAAYywEYgAQyCxAAGIAEGLEDGIMBMggQABjLARiABEjBgAFQugdYwHVwD3gAkAEDmAGJAqAB9QmqAQUwLjYuMrgBDMgBAPgBAagCCsICBxAjGOoCGCfCAhEQLhiABBixAxiDARjHARjRA8ICDhAuGIAEGLEDGMcBGNEDwgIIEAAYgAQYsQPCAggQABiKBRixA8ICBRAAGIAEwgILEC4YigUYsQMYgwHCAgsQLhiABBixAxiDAcICCxAAGIoFGLEDGIMBwgIIEAAYgAQYkgPCAggQABiABBjJAw&sclient=gws-wiz"
+      "https://www.google.com/search?q=%E0%B8%88%E0%B8%AD%E0%B8%87%E0%B8%95%E0%B8%B1%E0%B9%8B%E0%B8%A7%E0%B8%94%E0%B8%B9%E0%B8%AB%E0%B8%99%E0%B8%B1%E0%B8%87&sca_esv=571229774&sxsrf=AM9HkKm7z4MjuV9QPZXQ4z1BVW3B8466tA%3A1696583686668&source=hp&ei=BtAfZeunJtr71e8P7LC9oAc&iflsig=AO6bgOgAAAAAZR_eFuxmG-KIF4Z-S4i4yAtMYQO-WEn1&ved=0ahUKEwjrwuTfiuGBAxXaffUHHWxYD3QQ4dUDCAk&oq=%E0%B8%88%E0%B8%AD%E0%B8%87%E0%B8%95%E0%B8%B1%E0%B9%8B%E0%B8%A7%E0%B8%94%E0%B8%B9%E0%B8%AB%E0%B8%99%E0%B8%B1%E0%B8%87&gs_lp=Egdnd3Mtd2l6IifguIjguK3guIfguJXguLHguYvguKfguJTguLnguKvguJnguLHguIcyCBAAGMsBGIAEMggQABjLARiABDIIEAAYywEYgAQyCBAAGMsBGIAEMggQABjLARiABDIIEAAYywEYgAQyBBAAGB4yBBAAGB4yBBAAGB4yBBAAGB5IyEFQwRBY2CxwAXgAkAEAmAGpAaABwAyqAQQzLjEwuAEMyAEA-AEBqAIKwgIHECMY6gIYJ8ICBBAjGCfCAgwQIxiKBRgTGIAEGCfCAgUQABiABMICBRAuGIAEwgILEC4YgAQYxwEYrwE&sclient=gws-wiz"
     );
-    cy.get("#rso > div:nth-of-type(1) h3").click();
+    cy.get("#rso > div:nth-of-type(1) > div > div > div h3").click();
     cy.location("href").should(
       "eq",
-      "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwi75ID_4eCBAxXy1zgGHeUqCVYQFnoECAoQAQ&url=https%3A%2F%2Fctmusicshop.com%2Fproduct-category%2Fmusic-instrument%2Fguitar%2F&usg=AOvVaw2QrJ1GUjpxGVWzjEGrU4fk&opi=89978449"
+      "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwi17KfxiuGBAxUKzzgGHcjPACoQFnoECAsQAQ&url=https%3A%2F%2Fwww.majorcineplex.com%2F&usg=AOvVaw3zSrR_qmJ-EcFk7rK7lSTo&opi=89978449"
     );
   });
 });
