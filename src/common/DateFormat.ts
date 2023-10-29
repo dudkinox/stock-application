7c5239085dbecb5173e4bd861b3ed6028ffa7c99
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2023-10-30 01:09:55 +0700

    kay-stock :money_with_wings:

--- src/common/DateFormat.ts
+++ src/common/DateFormat.ts
@@ -1,12 +1,37 @@
 export default function convertDateToThai(date: Date): string {
-    const thaiMonths = [
-        'มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน',
-        'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'
-    ];
+  const thaiMonths = [
+    "มกราคม",
+    "กุมภาพันธ์",
+    "มีนาคม",
+    "เมษายน",
+    "พฤษภาคม",
+    "มิถุนายน",
+    "กรกฎาคม",
+    "สิงหาคม",
+    "กันยายน",
+    "ตุลาคม",
+    "พฤศจิกายน",
+    "ธันวาคม",
+  ];
 
-    const day = date.getDate();
-    const month = thaiMonths[date.getMonth()];
-    const year = date.getFullYear() + 543;
+  const day = date.getDate();
+  const month = thaiMonths[date.getMonth()];
+  const year = date.getFullYear() + 543;
 
-    return `วันที่ ${day} ${month} ${year}`;
+  return `วันที่ ${day} ${month} ${year}`;
+}
+export function convertToDateFormat(
+  inputDate: string | undefined | null
+): string {
+  if (!inputDate) {
+    return "";
+  }
+
+  const datePart = inputDate.split(" ")[0];
+  const dateObj = new Date(datePart);
+  const year = dateObj.getFullYear();
+  const month = String(dateObj.getMonth() + 1).padStart(2, "0");
+  const day = String(dateObj.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
 }
--- src/layouts/stock/KayMenuInsert.tsx
+++ src/layouts/stock/KayMenuInsert.tsx
@@ -8,15 +8,38 @@ import { AppContext } from "../../contexts";
 import { GetCustomerResponse } from "../../Models/Response/GetCustomerResponse";
 import CustomerServices from "../../services/CustomerServices";
 import StockService from "../../services/StockServices";
+import { convertToDateFormat } from "../../common/DateFormat";
+import TextInput from "../../common/TextInput";
 
 interface KayMenuInsertProps {
   id: string;
+  setEdit: React.Dispatch<
+    React.SetStateAction<{
+      stockType: string;
+      major: string;
+      payload: {};
+    }>
+  >;
+  edit: {
+    stockType: string;
+    major: string;
+    payload: any;
+  };
 }
 
-export default function KayMenuInsert({ id }: KayMenuInsertProps) {
+export default function KayMenuInsert({
+  id,
+  setEdit,
+  edit,
+}: KayMenuInsertProps) {
   const {
     setCustomerStatus,
     setCustomer,
+    setIdCard,
+    idCard,
+    setStockType,
+    date,
+    setDate,
     tel,
     setTel,
     version,
@@ -31,11 +54,6 @@ export default function KayMenuInsert({ id }: KayMenuInsertProps) {
     setInstallment,
     datePayment,
     setDatePayment,
-    setIdCard,
-    idCard,
-    date,
-    setDate,
-    setStockType,
   } = useContext(StockContext);
   const { setPathUrl, setIsLoading, majorUser } = useContext(AppContext);
   const [selectCustomer, setSelectCustomer] = useState<GetCustomerResponse[]>(
@@ -45,7 +63,8 @@ export default function KayMenuInsert({ id }: KayMenuInsertProps) {
     useState<GetCustomerResponse | null>(null);
   const navigate = useNavigate();
   const location = useLocation();
-  const insert = location.state.insert;
+  const insert = location.state?.insert;
+  const [isInsert, setIsInsert] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
@@ -85,21 +104,18 @@ export default function KayMenuInsert({ id }: KayMenuInsertProps) {
   }, [idCard, selectCustomer, customerExists, navigate, setPathUrl]);
 
   useEffect(() => {
+    setIsInsert(Number(id) !== 0);
+  }, []);
+
+  useEffect(() => {
     const major = sessionStorage.getItem("majorEdit");
     if (major) {
       StockService.GetFindStockById(id, major, "ขาย").then((res) => {
-        console.log(res.data);
-
-        // setCustomer(res.data.CUSTOMER_NAME);
-        // setTel(res.data.TEL);
-        // setVersion(res.data.VERSION);
-        // setImei(res.data.IMEI);
-        // setStarMoney(res.data.STAR_MONEY);
-        // setMonth(res.data.MONTH);
-        // setInstallment(res.data.INSTALLMENT);
-        // setDatePayment(res.data.DATE_PAYMENT);
-        // setDate(res.data.DATE);
-        // setIdCard(res.data.ID_CARD);
+        setEdit({
+          stockType: "ขาย",
+          major: major,
+          payload: res.data,
+        });
       });
     }
   }, []);
@@ -107,181 +123,200 @@ export default function KayMenuInsert({ id }: KayMenuInsertProps) {
   return (
     <>
       <span className="mt-3"></span>
-      <DataList
-        label={"ค้นหาชื่อ / เลือก เลขบัตรประชาชน:"}
-        setValue={setIdCard}
-        icon={"far fa-id-card"}
-        data={selectCustomer.map((item) => item)}
-        placeholder={"ค้นหาชื่อ / เลขบัตรประชาชน"}
-        minLength={13}
-        maxLength={13}
-        value={idCard}
+      {id === "" && (
+        <>
+          <DataList
+            label={"ค้นหาชื่อ / เลือก เลขบัตรประชาชน:"}
+            setValue={setIdCard}
+            icon={"far fa-id-card"}
+            data={selectCustomer.map((item) => item)}
+            placeholder={"ค้นหาชื่อ / เลขบัตรประชาชน"}
+            minLength={13}
+            maxLength={13}
+            value={idCard}
+          />
+          <div className="form-group">
+            <label className="float-left">{MenuKayEnum.CUSTOMER}</label>
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text">
+                  <i className="fas fa-user"></i>
+                </span>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                onChange={(e: any) => setCustomer(e.target.value)}
+                placeholder="ชื่อลูกค้า"
+                value={
+                  customerExists
+                    ? `${customerExists?.NAME} ${customerExists?.LAST_NAME}`
+                    : ""
+                }
+                readOnly={customerExists !== null}
+              />
+            </div>
+          </div>
+        </>
+      )}
+      <TextInput
+        label={MenuKayEnum.DATE}
+        icon={"fas fa-calendar"}
+        setValue={(e: any) =>
+          isInsert
+            ? setDate
+            : setEdit({
+                major: edit.major,
+                stockType: edit.stockType,
+                payload: {
+                  ...edit.payload,
+                  DATE: e,
+                },
+              })
+        }
+        type={"date"}
+        placeholder={"วันที่ขาย"}
+        value={isInsert ? date : edit.payload.DATE}
       />
-      <div className="form-group">
-        <label className="float-left">{MenuKayEnum.CUSTOMER}</label>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text">
-              <i className="fas fa-user"></i>
-            </span>
-          </div>
-          <input
-            type="text"
-            className="form-control"
-            onChange={(e: any) => setCustomer(e.target.value)}
-            placeholder="ชื่อลูกค้า"
-            value={
-              customerExists
-                ? `${customerExists?.NAME} ${customerExists?.LAST_NAME}`
-                : ""
-            }
-            readOnly={customerExists !== null}
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <label className="float-left">{MenuKayEnum.DATE}</label>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text">
-              <i className="fas fa-calendar"></i>
-            </span>
-          </div>
-          <input
-            type="date"
-            className="form-control"
-            onChange={(e: any) => setDate(e.target.value)}
-            placeholder="วันที่ขาย"
-            value={date}
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <label className="float-left">{MenuKayEnum.TEL}</label>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text">
-              <i className="fas fa-phone-alt"></i>
-            </span>
-          </div>
-          <input
-            type="tel"
-            className="form-control"
-            onChange={(e: any) => setTel(e.target.value)}
-            minLength={10}
-            maxLength={10}
-            placeholder="เบอร์โทร"
-            value={tel}
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <label className="float-left">{MenuKayEnum.VERSION}</label>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text">
-              <i className="fas fa-mobile"></i>
-            </span>
-          </div>
-          <input
-            type="text"
-            className="form-control"
-            onChange={(e: any) => setVersion(e.target.value)}
-            placeholder="รุ่น"
-            value={version}
-            readOnly
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <label className="float-left">{MenuKayEnum.IMEI}</label>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text">
-              <i className="fas fa-mobile"></i>
-            </span>
-          </div>
-          <input
-            type="text"
-            className="form-control"
-            onChange={(e: any) => setImei(e.target.value)}
-            placeholder="imei เครื่อง"
-            value={imei}
-            readOnly
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <label className="float-left">{MenuKayEnum.STAR_MONEY}</label>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text">
-              <i className="fas fa-money-check-alt"></i>
-            </span>
-          </div>
-          <input
-            type="number"
-            className="form-control"
-            onChange={(e: any) => setStarMoney(e.target.value)}
-            min={0}
-            placeholder="เงินดาวน์"
-            value={starMoney}
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <label className="float-left">{MenuKayEnum.MONTH}</label>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text">
-              <i className="fas fa-calendar"></i>
-            </span>
-          </div>
-          <input
-            type="number"
-            className="form-control"
-            onChange={(e: any) => setMonth(e.target.value)}
-            min={0}
-            placeholder="จำนวนเดือนที่ผ่อน"
-            value={month}
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <label className="float-left">{MenuKayEnum.INSTALLMENT}</label>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text">
-              <i className="fas fa-calendar-alt"></i>
-            </span>
-          </div>
-          <input
-            type="number"
-            className="form-control"
-            onChange={(e: any) => setInstallment(e.target.value)}
-            min={0}
-            placeholder="เดือนละ"
-            value={installment}
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <label className="float-left">{MenuKayEnum.DATE_PAYMENT}</label>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text">
-              <i className="fas fa-cash-register"></i>
-            </span>
-          </div>
-          <input
-            type="text"
-            className="form-control"
-            onChange={(e: any) => setDatePayment(e.target.value)}
-            placeholder="ชำระทุกวันที่"
-            value={datePayment}
-          />
-        </div>
-      </div>
+      <TextInput
+        label={MenuKayEnum.TEL}
+        icon={"fas fa-phone-alt"}
+        setValue={(e) =>
+          isInsert
+            ? setTel
+            : setEdit({
+                major: edit.major,
+                stockType: edit.stockType,
+                payload: {
+                  ...edit.payload,
+                  TEL: e,
+                },
+              })
+        }
+        type={"text"}
+        minLength={10}
+        maxLength={10}
+        placeholder="เบอร์โทร"
+        value={isInsert ? tel : edit.payload.TEL}
+      />
+      <TextInput
+        label={MenuKayEnum.VERSION}
+        icon={"fas fa-mobile"}
+        setValue={(e: any) =>
+          isInsert
+            ? setVersion
+            : setEdit({
+                major: edit.major,
+                stockType: edit.stockType,
+                payload: {
+                  ...edit.payload,
+                  VERSION: e,
+                },
+              })
+        }
+        type={"text"}
+        placeholder="รุ่น"
+        value={isInsert ? version : edit.payload.VERSION}
+        readonly={isInsert}
+      />
+      <TextInput
+        label={MenuKayEnum.IMEI}
+        icon={"fas fa-mobile"}
+        setValue={(e) =>
+          isInsert
+            ? setImei
+            : setEdit({
+                major: edit.major,
+                stockType: edit.stockType,
+                payload: {
+                  ...edit.payload,
+                  IMEI: e,
+                },
+              })
+        }
+        type={"text"}
+        placeholder={"imei เครื่อง"}
+        value={isInsert ? imei : edit.payload.IMEI}
+        readonly={isInsert}
+      />
+      <TextInput
+        label={MenuKayEnum.STAR_MONEY}
+        icon={"fas fa-money-check-alt"}
+        setValue={(e: any) =>
+          isInsert
+            ? setStarMoney
+            : setEdit({
+                major: edit.major,
+                stockType: edit.stockType,
+                payload: {
+                  ...edit.payload,
+                  STAR_MONEY: e,
+                },
+              })
+        }
+        type={"number"}
+        placeholder={"เงินดาวน์"}
+        value={isInsert ? starMoney : edit.payload.STAR_MONEY}
+      />
+      <TextInput
+        label={MenuKayEnum.MONTH}
+        icon={"fas fa-money-check-alt"}
+        setValue={(e: any) =>
+          isInsert
+            ? setMonth
+            : setEdit({
+                major: edit.major,
+                stockType: edit.stockType,
+                payload: {
+                  ...edit.payload,
+                  MONTH: e,
+                },
+              })
+        }
+        type="number"
+        min={0}
+        placeholder="จำนวนเดือนที่ผ่อน"
+        value={isInsert ? month : edit.payload.MONTH}
+      />
+      <TextInput
+        label={MenuKayEnum.INSTALLMENT}
+        icon={"fas fa-calendar-alt"}
+        setValue={(e: any) =>
+          isInsert
+            ? setInstallment
+            : setEdit({
+                major: edit.major,
+                stockType: edit.stockType,
+                payload: {
+                  ...edit.payload,
+                  INSTALLMENT: e,
+                },
+              })
+        }
+        type="number"
+        min={0}
+        placeholder="เดือนละ"
+        value={isInsert ? installment : edit.payload.INSTALLMENT}
+      />
+      <TextInput
+        label={MenuKayEnum.DATE_PAYMENT}
+        icon={"fas fa-cash-register"}
+        setValue={(e: any) =>
+          isInsert
+            ? setDatePayment
+            : setEdit({
+                major: edit.major,
+                stockType: edit.stockType,
+                payload: {
+                  ...edit.payload,
+                  DATE_PAYMENT: e,
+                },
+              })
+        }
+        type="text"
+        placeholder="ชำระทุกวันที่"
+        value={isInsert ? datePayment : edit.payload.DATE_PAYMENT}
+      />
     </>
   );
 }
