689921e5cf265015c6b099d654ad3ee9c5d3c450
กีตาร์เอาเอง <45527907+dudkinox@users.noreply.github.com>
2023-10-30 01:13:08 +0700

    Revert "Revert "asd""
    This reverts commit 7f6cca83f852e5bd1ce849a9ae9af903c6c2604d.

--- src/pages/Stock/add.tsx
+++ src/pages/Stock/add.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import ContentLayOut from "../../layouts/ContentLayOut";
 import { StockContext } from "../../contexts/StockContext";
 import IsMenuInsert from "../../layouts/stock/IsMenuInsert";
@@ -6,6 +6,8 @@ import ByeMenuInsert from "../../layouts/stock/ByeMenuInsert";
 import InstallmentMenuInsert from "../../layouts/stock/InstallmentMenuInsert";
 import KayMenuInsert from "../../layouts/stock/KayMenuInsert";
 import { useLocation } from "react-router-dom";
+import StockService from "../../services/StockServices";
+import { AlertError, AlertSuccess } from "../../common/ToastrCommon";
 
 export default function StockAddPage() {
   const { isMenuInsert, handlerSubmit } = useContext(StockContext);
@@ -13,6 +15,27 @@ export default function StockAddPage() {
   const id =
     new URLSearchParams(useLocation().search).get("id") ?? state.state.id;
   const addType = new URLSearchParams(useLocation().search).get("type");
+
+  const [edit, setEdit] = useState({
+    stockType: "",
+    major: "",
+    payload: {},
+  });
+
+  const updateHandlerSubmit = () => {
+    setIsLoading(true);
+
+    StockService.UpdateStock(id, edit.stockType, edit.payload, edit.major)
+      .then((res) => {
+        AlertSuccess("แก้ไขข้อมูลสำเร็จ");
+        setIsLoading(false);
+        // window.location.href = "/stock";
+      })
+      .catch((err) => {
+        AlertError(err.response.data.message);
+        setIsLoading(false);
+      });
+  };
 
   return (
     <ContentLayOut
@@ -23,7 +46,9 @@ export default function StockAddPage() {
       page={
         <>
           {isMenuInsert && <IsMenuInsert id={id} />}
-          {addType === "bye" && <ByeMenuInsert id={id} />}
+          {addType === "bye" && (
+            <ByeMenuInsert id={id} setEdit={setEdit} edit={edit} />
+          )}
           {addType === "kay" && <KayMenuInsert id={id} />}
           {addType === "equipment" && <IsMenuInsert id={id} />}
           {addType === "installment" && <InstallmentMenuInsert id={id} />}
@@ -31,7 +56,7 @@ export default function StockAddPage() {
             <button
               type="button"
               className="btn primary-btn col-3 my-3 "
-              onClick={handlerSubmit}
+              onClick={id === 0 ? handlerSubmit : updateHandlerSubmit}
             >
               บันทึก
             </button>
@@ -40,4 +65,7 @@ export default function StockAddPage() {
       }
     />
   );
+}
+function setIsLoading(arg0: boolean) {
+  throw new Error("Function not implemented.");
 }
