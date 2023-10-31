efa45cefe889f925f298a49c43d5d358eb83606c
กีตาร์เอาเอง <45527907+dudkinox@users.noreply.github.com>
2023-10-31 10:47:11 +0700

    asd

--- src/pages/Stock/InstallmentPaymentPage.tsx
+++ src/pages/Stock/InstallmentPaymentPage.tsx
@@ -107,114 +107,178 @@ export function StockInstallmentPaymentPage() {
   return (
     <>
       <ContentLayOut
-        title={"stock"}
-        topic={"ผ่อน"}
-        btnHeader={
-          <button
-            onClick={() => {
-              setDate("");
-              setIdCard("");
-              setIsMenuInsert(false);
-              setByeMenuInsert(false);
-              setKayMenuInsert(false);
-              setNewInstallmentMenuInsert(false);
-              clearInputValue();
-            }}
-            className="btn primary-btn text-white float-right"
-            data-toggle="modal"
-            data-target="#insert-modal"
-            id="insert-customer"
-          >
-            เพิ่มข้อมูลผ่อน
-          </button>
-        }
+        title={"ผ่อน"}
+        topic={""}
         page={
           <>
-            <ModalCommon
-              title={"เพิ่มข้อมูลผ่อน"}
-              id={"insert-modal"}
-              content={
-                <>
-                  <div className="modal-body">
-                    <div className="container-fluid">
-                      {isEdit() && majorUser === "admin" && (
-                        <SelectChoice
-                          topic="เลือกสาขา"
-                          setValue={setMajorInsert}
-                          icon="far fa-calendar-alt"
-                          label={"สาขา:"}
-                          value={majorInsert}
-                          options={fetchMajor.map((item) => item.NAME)}
-                        />
-                      )}
-                      <TextInput
-                        label={"วันที่:"}
-                        icon={"far fa-calendar-alt"}
-                        setValue={setDate}
-                        type={"date"}
-                        value={date}
-                      />
-                      <TextInput
-                        label={"ประเภท"}
-                        setValue={setStockType}
-                        icon={"far fa-file"}
-                        type={"text"}
-                        value={stockType}
-                        readonly={true}
-                      />
-                    </div>
-                  </div>
-                  <div className="modal-footer">
-                    <button
-                      type="button"
-                      className="btn primary-btn col-lg-2 col-sm-auto"
-                      data-dismiss="modal"
-                      onClick={nextValidate}
+            <div className="card card-primary card-outline card-tabs">
+              <div className="card-header p-0 pt-1 border-bottom-0">
+                <ul
+                  className="nav nav-tabs"
+                  id="custom-tabs-three-tab"
+                  role="tablist"
+                >
+                  <li className="nav-item">
+                    <a
+                      className="nav-link active"
+                      id="custom-tabs-three-home-tab"
+                      data-toggle="pill"
+                      href="#custom-tabs-three-home"
+                      role="tab"
+                      aria-controls="custom-tabs-three-home"
+                      aria-selected="true"
                     >
-                      ถัดไป
-                    </button>
-                  </div>
-                </>
-              }
-            />
-            <div className="card-body">
-              <TableCommon
-                columns={stockTableHeaders}
-                row={stock.map((item, i) => (
-                  <tr key={i} className="text-center">
-                    <td>{item.ID_CARD}</td>
-                    <td>{convertDateToThai(new Date(item.DATE))}</td>
-                    <td>{item.MAJOR}</td>
-                    <td>{item.INSTALLMENT_NO}</td>
-                    <td>{Number(item.PRICE_TOTAL).toLocaleString()} บาท</td>
-                    <td>
-                      <button
-                        type="button"
-                        className="btn btn-warning"
-                        onClick={() =>
-                          handlerInstallment(
-                            item.ID,
-                            item.MAJOR,
-                            item.INSTALLMENT_NO,
-                            item.PRICE_TOTAL
-                          )
+                      ตารางข้อมูลผ่อน
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      id="custom-tabs-three-profile-tab"
+                      data-toggle="pill"
+                      href="#custom-tabs-three-profile"
+                      role="tab"
+                      aria-controls="custom-tabs-three-profile"
+                      aria-selected="false"
+                    >
+                      ตารางเก็บเงิน
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="card-header">
+                <h2 className="card-title mt-2">
+                  {"เพิ่ม / ลบ / แก้ไข / ค้นหา"}
+                </h2>
+                <button
+                  onClick={() => {
+                    setDate("");
+                    setIdCard("");
+                    setIsMenuInsert(false);
+                    setByeMenuInsert(false);
+                    setKayMenuInsert(false);
+                    setNewInstallmentMenuInsert(false);
+                    clearInputValue();
+                  }}
+                  className="btn primary-btn text-white float-right"
+                  data-toggle="modal"
+                  data-target="#insert-modal"
+                  id="insert-customer"
+                >
+                  เพิ่มข้อมูลผ่อน
+                </button>
+              </div>
+              <div className="card-body">
+                <div className="tab-content" id="custom-tabs-three-tabContent">
+                  <div
+                    className="tab-pane fade show active"
+                    id="custom-tabs-three-home"
+                    role="tabpanel"
+                    aria-labelledby="custom-tabs-three-home-tab"
+                  >
+                    <>
+                      <ModalCommon
+                        title={"เพิ่มข้อมูลผ่อน"}
+                        id={"insert-modal"}
+                        content={
+                          <>
+                            <div className="modal-body">
+                              <div className="container-fluid">
+                                {isEdit() && majorUser === "admin" && (
+                                  <SelectChoice
+                                    topic="เลือกสาขา"
+                                    setValue={setMajorInsert}
+                                    icon="far fa-calendar-alt"
+                                    label={"สาขา:"}
+                                    value={majorInsert}
+                                    options={fetchMajor.map(
+                                      (item) => item.NAME
+                                    )}
+                                  />
+                                )}
+                                <TextInput
+                                  label={"วันที่:"}
+                                  icon={"far fa-calendar-alt"}
+                                  setValue={setDate}
+                                  type={"date"}
+                                  value={date}
+                                />
+                                <TextInput
+                                  label={"ประเภท"}
+                                  setValue={setStockType}
+                                  icon={"far fa-file"}
+                                  type={"text"}
+                                  value={stockType}
+                                  readonly={true}
+                                />
+                              </div>
+                            </div>
+                            <div className="modal-footer">
+                              <button
+                                type="button"
+                                className="btn primary-btn col-lg-2 col-sm-auto"
+                                data-dismiss="modal"
+                                onClick={nextValidate}
+                              >
+                                ถัดไป
+                              </button>
+                            </div>
+                          </>
                         }
-                      >
-                        แก้ไข
-                      </button>
-                    </td>
-                    <td>
-                      <button
-                        type="button"
-                        className="btn btn-danger"
-                        onClick={deleteStock(item.ID, item.MAJOR)}
-                      >
-                        ลบ
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              />
+                      />
+                      <div className="card-body">
+                        <TableCommon
+                          columns={stockTableHeaders}
+                          row={stock.map((item, i) => (
+                            <tr key={i} className="text-center">
+                              <td>{item.ID_CARD}</td>
+                              <td>{convertDateToThai(new Date(item.DATE))}</td>
+                              <td>{item.MAJOR}</td>
+                              <td>{item.INSTALLMENT_NO}</td>
+                              <td>
+                                {Number(item.PRICE_TOTAL).toLocaleString()} บาท
+                              </td>
+                              <td>
+                                <button
+                                  type="button"
+                                  className="btn btn-warning"
+                                  onClick={() =>
+                                    handlerInstallment(
+                                      item.ID,
+                                      item.MAJOR,
+                                      item.INSTALLMENT_NO,
+                                      item.PRICE_TOTAL
+                                    )
+                                  }
+                                >
+                                  แก้ไข
+                                </button>
+                              </td>
+                              <td>
+                                <button
+                                  type="button"
+                                  className="btn btn-danger"
+                                  onClick={deleteStock(item.ID, item.MAJOR)}
+                                >
+                                  ลบ
+                                </button>
+                              </td>
+                            </tr>
+                          ))}
+                        />
+                      </div>
+                    </>
+                  </div>
+                  <div
+                    className="tab-pane fade"
+                    id="custom-tabs-three-profile"
+                    role="tabpanel"
+                    aria-labelledby="custom-tabs-three-profile-tab"
+                  >
+                    test
+                  </div>
+                </div>
+              </div>
             </div>
           </>
         }
